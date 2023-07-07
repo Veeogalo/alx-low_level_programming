3648a828dd2bcd1e941cc2035e6a7b05a100c6e9
Veronica_Ogalo <veronica_ogalo@example.com>
2023-07-07 13:34:24 -0700

    a function that prints a string

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
-*  _puts_recursion - prints a string
+*_puts_recursion - prints a string
 *@s: input
 *Return: Always 0
 */
@@ -11,5 +11,6 @@ if (*s)
 _putchar(*s);
 _puts_recursion(s + 1);
 }
+else
 _putchar('\n');
 }
